d768187567fe7c0994a0c3944ee8c0df83265cf0
Andrew <shebanoff.andrei@yandex.ru>
2022-04-19 21:21:28 +0700

    used clang-format

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 using namespace std;
@@ -8,46 +8,41 @@ int main()
     string line;
     string Slices[500];
     ifstream input("input.txt"); // окрываем файл для чтения
-    if (input.is_open())
-    {
-        int w=0;
+    if (input.is_open()) {
+        int w = 0;
         int k = 0;
-        while (getline(input, line))
-        {
-
-
-            for(int i=0 ; i<line.length()-1; i++){
-                if(line[i]==' ' || line[i]==')' || line[i]=='(' || line[i]==',') {
-
-                   i++;
+        while (getline(input, line)) {
+            for (int i = 0; i < line.length() - 1; i++) {
+                if (line[i] == ' ' || line[i] == ')' || line[i] == '('
+                    || line[i] == ',') {
+                    i++;
                 } else {
-                    int q=0;
+                    int q = 0;
                     string slice = "";
 
-                    while(line[i+q]!=' ' && line[i+q]!='(' && line[i+q]!=')' && line[i+q]!=','){
-                        slice += line[i+q];
+                    while (line[i + q] != ' ' && line[i + q] != '('
+                           && line[i + q] != ')' && line[i + q] != ',') {
+                        slice += line[i + q];
 
                         q++;
-
                     }
-                    Slices[k]=slice;
+                    Slices[k] = slice;
                     k++;
-                    i+=q;
+                    i += q;
                     cout << slice << endl;
                 }
             }
-            if(Slices[w]=="circle"){
+            if (Slices[w] == "circle") {
                 cout << "it's circle" << endl;
             } else {
                 cout << "error, it's not a circle" << endl;
                 return 1;
             }
 
-            w+=4;
-
+            w += 4;
         }
     }
-    input.close();     // закрываем файл
+    input.close(); // закрываем файл
 
     cout << "End of program" << endl;
     return 0;
